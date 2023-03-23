365ff236275e62c0d023070e33d382772ab29367
milazar <milazar@host.docker.internal>
2023-03-23 11:28:08 +0100

    Tests repetidos

--- demo/src/test/java/com/example/ejemplos/PersonaTest.java
+++ demo/src/test/java/com/example/ejemplos/PersonaTest.java
@@ -3,6 +3,9 @@ package com.example.ejemplos;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 
 class PersonaTest {
@@ -25,5 +28,24 @@ class PersonaTest {
 		
 		
 	}
+	
+	@RepeatedTest(value = 5, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+	@DisplayName("Test multiples creaciones")
+		void repeatedTestCreate(RepetitionInfo repetitionInfo) {
+		var p = Persona.builder().id(repetitionInfo.getCurrentRepetition())
+				.nombre("Pepito" + repetitionInfo.getCurrentRepetition())
+				.apellidos("Grillo").build();
+		
+		assertNotNull(p);
+		assertTrue(p instanceof Persona, "No es una instancia de persona");
+		
+		assertAll("Validar propiedades", 
+				() -> assertEquals(repetitionInfo.getCurrentRepetition(), p.getId(), "id"),
+				() -> assertEquals("Pepito" + repetitionInfo.getCurrentRepetition(), p.getNombre(), "getNombre"),
+				() -> assertEquals("Grillo", p.getApellidos(), "getApellidos"));
+		
+	}
+	
+
 
 }
