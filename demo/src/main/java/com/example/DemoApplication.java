98ed01ab3aad6624191ec2729ca8087afb843034
milazar <milazar@host.docker.internal>
2023-03-28 13:11:35 +0200

    add class entities y pruebas.

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,29 +1,15 @@
 package com.example;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowMapper;
+import org.springframework.data.domain.Sort;
 
-import com.example.ioc.Rango;
-import com.example.ioc.StringRepository;
-import com.example.ioc.StringRepositoryImpl;
-import com.example.ioc.StringRepositoryMockImpl;
-import com.example.ioc.StringService;
-import com.example.ioc.StringServiceImpl;
-import com.example.ioc.UnaTonteria;
+import com.example.domains.contracts.repositories.ActorRepository;
+import com.example.domains.entities.Actor;
+import com.example.ioc.EjemplosIoC;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -31,69 +17,28 @@ public class DemoApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-	@Autowired
-	@Qualifier("Remoto")
-	private StringService srv;
 	
 	@Autowired
-	@Qualifier("Local")
-	@Lazy
-	private StringService srvLocal;
-	
-	@Value("${mi.valor:(Sin valor)}")
-	private String config;
-	
-	@Autowired
-	Rango rango;
-	
-	@Autowired(required = false)
-	UnaTonteria tonteria;
-
-	@Autowired
-	JdbcTemplate jdbcTemplate;
-
-	@Data @AllArgsConstructor
-	class Actor {
-		private int id;
-		private String first_name, last_name;
-	}
-	class ActorRowMapper implements RowMapper<Actor> {
-	      @Override
-	      public Actor mapRow(ResultSet rs, int rowNum) throws SQLException {
-	            return new Actor(rs.getInt("actor_id"), rs.getString("last_name"), rs.getString("first_name"));
-	      }
-	}
+	ActorRepository daoActorRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Aplicación arrancada");
+//		(new EjemplosIoC()).run();
+//		var actor = new Actor(0,"Pepito", "Grillo");
+//		daoActorRepository.save(actor);
+//		var item = daoActorRepository.findById(201);
+//		if(item.isPresent()) {
+//			var actor = item.get();
+//			actor.setLastName(actor.getLastName().toUpperCase());
+//			daoActorRepository.save(actor);
+//		}else {
+//			System.out.println("Actor no encontrado");
+//		}
+//		daoActorRepository.findAll().forEach(System.out::println);
 		
-//		StringRepository dao = new StringRepositoryImpl();
-//		dao = new StringRepositoryMockImpl();
-//		var srv = new StringServiceImpl(dao);
-//		System.out.println(srv.get(1));
-//		System.out.println(srvLocal.get(1));
-//		srv.add("Este es el remoto");
-//		srvLocal.add("Este es el local");
-//		srv.add("Este es el remoto");
-//		srvLocal.add("Este es el local");
-//		System.out.println(rango.toString());
-//		System.out.println(rango.getMin() + rango.getMax());
-//		System.out.println(tonteria != null ? tonteria.dimeAlgo() : "Tonteria nula");
-//		System.out.println(config);
-//		srv.add("algo");
-		var lst = jdbcTemplate.query("""
-				SELECT actor_id, first_name, last_name
-				from actor
-				""", new ActorRowMapper()
-				);
-		//lst.forEach(System.out::println);
-		jdbcTemplate.queryForList("""
-				SELECT concat(first_name, ' ', last_name)
-				from actor
-				""", String.class).forEach(System.out::println);
-
+		daoActorRepository.findTop5ByFirstNameStartingWithOrderByLastNameDesc("P").forEach(System.out::println);
+		daoActorRepository.findTop5ByFirstNameStartingWith("P", Sort.by("LastName").descending()).forEach(System.out::println);
+		daoActorRepository.findTop5ByFirstNameStartingWith("P", Sort.by("LastName")).forEach(System.out::println);
 	}
-
 }
---
+++ demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
@@ -0,0 +1,15 @@
+package com.example.domains.contracts.repositories;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.domains.entities.Actor;
+
+public interface ActorRepository extends JpaRepository<Actor, Integer>{
+	
+	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefix);
+	List<Actor> findTop5ByFirstNameStartingWith(String prefix, Sort order);
+
+}
