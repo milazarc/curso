430ee4ae5da3809bd9ed1c09145e9fd8944b7e1c
milazar <milazar@host.docker.internal>
2023-03-29 13:23:49 +0200

    Proyecciones mas Json

--- demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
+++ demo/src/main/java/com/example/domains/contracts/repositories/ActorRepository.java
@@ -9,19 +9,23 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.example.domains.entities.Actor;
+import com.example.domains.entities.dtos.ActorDTO;
+import com.example.domains.entities.dtos.ActorShort;
 
-public interface ActorRepository extends JpaRepository<Actor, Integer>, JpaSpecificationExecutor<Actor>{
-	
-	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefix);
-	List<Actor> findTop5ByFirstNameStartingWith(String prefix, Sort order);
+
+public interface ActorRepository extends JpaRepository<Actor, Integer>, JpaSpecificationExecutor<Actor> {
+	List<Actor> findTop5ByFirstNameStartingWithOrderByLastNameDesc(String prefijo);
+	List<Actor> findTop5ByFirstNameStartingWith(String prefijo, Sort orden);
+//	List<Actor> findTop5ByNombre(String prefijo, Sort orden);
+
+	List<ActorShort> findByActorIdNotNull();
+
+	<T> List<T> findAllBy(Class<T> type);
 	
 	@Query("SELECT a FROM Actor a WHERE a.actorId < :id")
 	List<Actor> findConJPQL(@Param("id") int actorId);
-	
 	@Query(name = "Actor.findAll")
 	List<Actor> findConJPQL();
-	
-	@Query(value = "SELECT * FROM actor WHERE actor_id < ?1", nativeQuery = true)
-	List<Actor> findConSQL(int actorID);
-
+	@Query(value = "SELECT * from actor WHERE actor_id < ?1", nativeQuery = true)
+	List<Actor> findConSQL(int actorId);
 }
