7b32c4b5c5f6ba6164b20a0a1726bac7930bf143
milazar <milazar@host.docker.internal>
2023-03-31 11:55:10 +0200

    Añadido DTos de film-catalog y servicio

--- demo/src/test/java/com/example/domains/contracts/repositories/ActorRepositoryMemoryTest.java
+++ demo/src/test/java/com/example/domains/contracts/repositories/ActorRepositoryMemoryTest.java
@@ -2,6 +2,8 @@ package com.example.domains.contracts.repositories;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.Timestamp;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,9 +24,17 @@ class ActorRepositoryMemoryTest {
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		em.persist(new Actor(0, "Pepito", "GRILLO	"));
-		em.persist(new Actor(0, "Carmelo", "COTON"));
-		em.persist(new Actor(0, "Capitan", "TAN"));
+		var item = new Actor(0, "Pepito", "GRILLO");
+		item.setLastUpdate(Timestamp.valueOf("2019-01-01 00:00.00"));
+		em.persist(item);
+		
+		item = new Actor(0, "Carmelo", "COTON");
+		item.setLastUpdate(Timestamp.valueOf("2019-01-01 00:00.00"));
+		em.persist(item);
+		
+		item = new Actor(0, "Capitan", "TAN");
+		item.setLastUpdate(Timestamp.valueOf("2019-01-01 00:00.00"));
+		em.persist(item);
 	}
 	
 	
