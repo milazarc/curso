f466d5c50be3377293dcc125daa5fa327a068048
milazar <milazar@host.docker.internal>
2023-03-31 14:24:24 +0200

    Creado ActorShortDTO

--- film-catalog/src/main/java/com/example/domains/entities/dtos/ActorShort.java
+++
@@ -1,9 +0,0 @@
-package com.example.domains.entities.dtos;
-
-import org.springframework.beans.factory.annotation.Value;
-
-public interface ActorShort {
-	int getActorId();
-	@Value("#{target.firstName + ' ' + target.lastName}")
-	String getNombre();
-}
