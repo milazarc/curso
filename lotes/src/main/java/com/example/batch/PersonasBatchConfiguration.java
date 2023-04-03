5f611f3acbe8f75b95e0c87877ca052fadc318a5
milazar <milazar@host.docker.internal>
2023-04-03 12:17:47 +0200

    Nuevas funciones

---
+++ lotes/src/main/java/com/example/batch/PersonasBatchConfiguration.java
@@ -0,0 +1,62 @@
+package com.example.batch;
+
+import javax.sql.DataSource;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.transaction.PlatformTransactionManager;
+import com.example.model.Persona;
+import com.example.model.PersonaDTO;
+
+@Configuration
+public class PersonasBatchConfiguration {
+	@Autowired
+	JobRepository jobRepository;
+	@Autowired
+	PlatformTransactionManager transactionManager;
+
+	public FlatFileItemReader<PersonaDTO> personaCSVItemReader(String fname) {
+		return new FlatFileItemReaderBuilder<PersonaDTO>().name("personaCSVItemReader")
+				.resource(new ClassPathResource(fname)).linesToSkip(1).delimited() // delimitador por defecto ;, de lo
+																					// contrario entre parentesis
+																					// "elemento"
+				.names(new String[] { "id", "nombre", "apellidos", "correo", "sexo", "ip" })
+				.fieldSetMapper(new BeanWrapperFieldSetMapper<PersonaDTO>() {
+					{
+						setTargetType(PersonaDTO.class);
+					}
+				}).build();
+	}
+
+	@Autowired
+	public PersonaItemProcessor personaItemProcessor;
+
+	@Bean
+	public JdbcBatchItemWriter<Persona> personaDBItemWriter(DataSource dataSource) {
+		return new JdbcBatchItemWriterBuilder<Persona>()
+				.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+				.sql("INSERT INTO personas VALUES (:id,:nombre,:correo,:ip)").dataSource(dataSource).build();
+	}
+}
