ab87e8e11732dcdbbc2d20d15a48c27167bdb2cb
milazar <milazar@host.docker.internal>
2023-04-05 14:41:53 +0200

    MVC

---
+++ demo/src/main/java/com/example/ApiExceptionHandler.java
@@ -0,0 +1,67 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.example.exceptions.BadRequestException;
+import com.example.exceptions.DuplicateKeyException;
+import com.example.exceptions.InvalidDataException;
+import com.example.exceptions.NotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.Serializable;
+
+import org.springframework.http.HttpStatus;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+	public static class ErrorMessage implements Serializable {
+		private static final long serialVersionUID = 1L;
+		private String error, message;
+
+		public ErrorMessage(String error, String message) {
+			this.error = error;
+			this.message = message;
+		}
+
+		public String getError() {
+			return error;
+		}
+
+		public void setError(String error) {
+			this.error = error;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class})
+    @ResponseBody
+    public ErrorMessage notFoundRequest(HttpServletRequest request, Exception exception) {
+        return new ErrorMessage(exception.getMessage(), request.getRequestURI());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({ BadRequestException.class, DuplicateKeyException.class })
+    @ResponseBody
+    public ErrorMessage badRequest(Exception exception) {
+        return new ErrorMessage(exception.getMessage(), "");
+    }
+    
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({ InvalidDataException.class })
+    @ResponseBody
+    public ErrorMessage invalidData(Exception exception) {
+        return new ErrorMessage("Invalid data",exception.getMessage());
+    }
+}
